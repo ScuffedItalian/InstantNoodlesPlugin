9a7a9a31d0b32b5b59035005ddd5307487a6399c
ScuffedGuy <110801525+ScuffedItalian@users.noreply.github.com>
2023-01-08 23:59:34 -0800

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+namespace RequirmentPlus
+{
+    [Plugin("InstantNoodles")]
+    public class Plugin
+    {
+        [Init]
+        
+        private void Init()
+        {
+            Console.WriteLine("Plugin has loaded!");
+            ExtensionButtons.AddButton(null, "Add Noodle As A Requirment", OnBttnClick1);
+      
+
+
+        }
+
+        private void OnBttnClick1() => RequirementCheck.RegisterRequirement(new ReqClass());
+      
+
+
+        [Exit]
+        private void Exit()
+        {
+            Console.WriteLine("Application has closed!");
+        }
+    }
+}
---
+++ ReqClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequirmentPlus
+{
+    public class ReqClass : RequirementCheck
+    {
+        public override string Name => "Noodle Extensions";
+
+        public override RequirementType IsRequiredOrSuggested(BeatSaberSong.DifficultyBeatmap mapInfo, BeatSaberMap map)
+        {
+            return RequirementType.Requirement;
+        }
+    }
+}
